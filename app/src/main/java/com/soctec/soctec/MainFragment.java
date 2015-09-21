1d66ad249db719ef4c81ba61c105d9f8d03c87c4
Jesper <jeskje@student.chalmers.se>
2015-09-21 16:30:14 +0200

    MainFragment XML and JAVA class

---
+++ app/src/main/java/com/soctec/soctec/MainFragment.java
@@ -0,0 +1,23 @@
+package com.soctec.soctec;
+
+/**
+ * Created by Jeppe on 2015-09-21.
+ */
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.View;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+
+public class MainFragment extends Fragment
+{
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+    {
+        View view = inflater.inflate(R.layout.fragment_main, container, false );
+        return view;
+    }
+}
