4e9c12515875b17d5ee9f9bd8ee62f679953e0c7
Jesper <jeskje@student.chalmers.se>
2015-09-22 14:48:48 +0200

    Minor fixes, added a onClick function to the camera button which starts the method scanNow

--- app/src/main/java/com/soctec/soctec/MainFragment.java
+++ app/src/main/java/com/soctec/soctec/MainFragment.java
@@ -4,6 +4,7 @@ package com.soctec.soctec;
  * Created by Jeppe on 2015-09-21.
  */
 
+import android.content.DialogInterface;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
