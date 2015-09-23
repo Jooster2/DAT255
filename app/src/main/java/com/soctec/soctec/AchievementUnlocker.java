277798c2d2288ad96dea848563c25d7297b80b8d
RallaRen <chult@student.chalmers.se>
2015-09-23 12:55:13 +0200

    Added logic
    Added one get-method in CounterAchievement.java and some added logic in
    unlocker.

--- app/src/main/java/com/soctec/soctec/AchievementUnlocker.java
+++ app/src/main/java/com/soctec/soctec/AchievementUnlocker.java
@@ -1,22 +1,59 @@
 package com.soctec.soctec;
 
+import android.support.annotation.NonNull;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
 import java.util.List;
+import java.util.ListIterator;
+import java.util.Observable;
 /**
  * Created by Carl-Henrik Hult on 2015-09-22.
  */
-public class AchievementUnlocker
+
+
+public class AchievementUnlocker extends Observable
 {
-    List<Achievement> unlockableCounterAchievements;
-    List<Achievement> unlockableCollectionAchievements;
+    public static final int SCAN_PERSON = 1;
+
+    ArrayList<Achievement> unlockableAchievements;
     //Stats currentStats;
 
     public AchievementUnlocker()
     {
+        unlockableAchievements = new ArrayList<Achievement>();
 
     }
-
-
     public void insertAchievement(Achievement achievement)
     {
+        unlockableAchievements.add(achievement);
+    }
+    public void receiveEvent (int type, String content)
+    {
+        if (type == SCAN_PERSON)
+        {
+            Stats.setLastScanned (content);
+            Stats.incScanCount();
+
+            for (Achievement achievement : unlockableAchievements)
+            {
+                CounterAchievement cAchievement = (CounterAchievement) achievement;
+                boolean achievementUnlocked = true;
+                for(CounterDemand demand : cAchievement.getDemands())
+                {
+                    if(demand.type.equals("SCAN"))
+                    {
+                        if(demand.amount > Stats.getScanCount())
+                        {
+                            achievementUnlocked = false;
+                            break;
+                        }
+                    }
+                }
+                if (achievementUnlocked == true)
+                    Stats.addUnlockedAchievement (cAchievement);
+            }
+        }
     }
 }
