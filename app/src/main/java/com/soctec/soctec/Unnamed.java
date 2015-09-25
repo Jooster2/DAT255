cbf1694d70037d167efb484ceac0a15453270eee
Punell <Punell@student.chalmers.se>
2015-09-25 15:47:54 +0200

    updated QRGen

--- app/src/main/java/com/soctec/soctec/Unnamed.java
+++ app/src/main/java/com/soctec/soctec/Unnamed.java
@@ -1,41 +1,40 @@
 package com.soctec.soctec;
 
+import android.graphics.Bitmap;
+
 
 public class Unnamed
 {
-
+    private Encryptor encryptor;
+    private QRGen qrGen;
 
     public Unnamed()
     {
-        Encryptor encryptor = new Encryptor();
-        QRgen qrgen = new QRgen();
+        encryptor = new Encryptor();
+        qrGen = new QRGen();
     }
 
 
     public String getPlayAcc()
     {
-
+        //dummy
         return "abc@abc.com";
     }
 
     public String getUserIP()
     {
-
+        //dummy
         return "192.168.0.1";
     }
 
+    public Bitmap getQrOfIp()
+    {
+        return qrGen.getQR(getUserIP());
+    }
 
-
-
-
-
-
-
-
-
-
-
-
-
+    public String getEncryptedAcc()
+    {
+        return encryptor.encrypt(getPlayAcc());
+    }
 
 }
