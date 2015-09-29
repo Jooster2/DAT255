51d0512a0b0b5106cfb99a436b1acd62477c93be
Joakim Schmidt <joakim.schmidt@gmail.com>
2015-09-29 12:46:04 +0200

    Major changes to Profile Added ProfileEdit and XML, added image for button, changed ProfileAdapter

--- app/src/main/java/com/soctec/soctec/ProfileAdapter.java
+++ app/src/main/java/com/soctec/soctec/ProfileAdapter.java
@@ -5,27 +5,30 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import java.util.ArrayList;
 /**
- * Created by Carl-Henrik Hult on 2015-09-25.
+ * Created by Jooster on 2015-09-29.
  */
 public class ProfileAdapter extends ArrayAdapter
 {
-    public ProfileAdapter(Context context, ArrayList<String> matchResult)
+    public ProfileAdapter(Context context, ArrayList<String> contents)
     {
-        super(context, R.layout.row_profile);
+        super(context, R.layout.row_profile, contents);
 
     }
     @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
-        LayoutInflater myInflater = LayoutInflater.from(getContext());
-        View customView = myInflater.inflate(R.layout.row_profile, parent, false);
-        String singleStringItem = (String) getItem (position);
-        TextView title = (TextView)customView.findViewById(R.id.matchWindowTitle);
-        TextView content = (TextView)customView.findViewById(R.id.matchWindowContent);
-        String [] strings = singleStringItem.split(",");
+    public View getView(int position, View convertView, ViewGroup parent)
+    {
+        LayoutInflater inflater = LayoutInflater.from(getContext());
+        View customView = inflater.inflate(R.layout.row_profile, parent, false);
+        String item = (String)getItem(position);
+        TextView title = (TextView)customView.findViewById(R.id.titleText);
+        TextView content = (TextView)customView.findViewById(R.id.contentText);
+        ImageButton editButton = (ImageButton)customView.findViewById(R.id.editButton);
+        String[] strings = item.split(":");
 
         title.setText(strings[0]);
         content.setText(strings[1]);
