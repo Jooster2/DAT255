828d3294da9b33f4dc9e26bf722a449e27369a36
Punell <Punell@student.chalmers.se>
2015-10-01 16:40:18 +0200

    draft

--- app/src/main/java/com/soctec/soctec/core/AchievementShowerActivity.java
+++ app/src/main/java/com/soctec/soctec/core/AchievementShowerActivity.java
@@ -7,18 +7,38 @@ import android.view.MenuItem;
 import android.app.Activity;
 import android.view.View;
 import android.widget.Button;
-
+import android.widget.ImageView;
 
 import com.soctec.soctec.R;
 
 public class AchievementShowerActivity extends Activity
 {
+    private Image mainImageBack;
+    private Image mainImageFront;
+    private Image headText;
+    private int headTextId;
+    private int frontImageId;
+    private int backImageId;
+
+    //private Image button;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_achievement_shower);
+
+        ImageView headImage = (ImageView)findViewById(R.id.showerHeadView);
+        headImage.setImageResource(headTextId);
+
+
+        ImageView mainImageBackView = (ImageView)findViewById(R.id.showerAchViewBack);
+        mainImageBackView.setImageResource(backImageId);
+
+
+        ImageView mainImageFrontView = (ImageView)findViewById(R.id.showerAchViewFront);
+        mainImageFrontView.setImageResource(frontImageId);
 
 
         Button quitButton = (Button) findViewById(R.id.showerQuitButton);
@@ -37,14 +57,15 @@ public class AchievementShowerActivity extends Activity
 
 
 
-    public void getAchievementImage() // Image
+    public int getAchievementImageId()
     {
-
+        return 1;
     }
     public void getHeadText()
     {
 
     }
+
     public void getAchievementPoints()
     {
 
