23a159620ceb15e02bcebca9faac5d32c4cc0a48
Jesper <jeskje@student.chalmers.se>
2015-10-06 21:05:08 +0200

    Simple List using ListFragment with a ViewSwitcher switching between lists in the Fragment

--- app/src/main/java/com/soctec/soctec/core/AchievementsFragment.java
+++ app/src/main/java/com/soctec/soctec/core/AchievementsFragment.java
@@ -10,16 +10,43 @@ import android.support.annotation.Nullable;
 import android.view.View;
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ViewSwitcher;
 
 import com.soctec.soctec.R;
 
 public class AchievementsFragment extends Fragment
 {
+    Button unlocked, locked;
+    ViewSwitcher viewSwitcher;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
     {
-        View view = inflater.inflate(R.layout.fragment_achievements, container, false );
+        View view = inflater.inflate(R.layout.fragment_achievements, container, false);
+
+        unlocked = (Button) view.findViewById(R.id.unlocked);
+        locked = (Button) view.findViewById(R.id.locked);
+        viewSwitcher = (ViewSwitcher) view.findViewById(R.id.viewSwitcher);
+
+        unlocked.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                viewSwitcher.showPrevious();
+            }
+        });
+
+        locked.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                viewSwitcher.showNext();
+            }
+        });
         return view;
     }
 }
