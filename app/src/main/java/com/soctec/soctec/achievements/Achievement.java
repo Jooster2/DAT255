b6499554bf68592ce0db11c82344185cf4339016
Punell <Punell@student.chalmers.se>
2015-10-06 13:09:39 +0200

    serialVersionUID

--- app/src/main/java/com/soctec/soctec/achievements/Achievement.java
+++ app/src/main/java/com/soctec/soctec/achievements/Achievement.java
@@ -9,6 +9,7 @@ import java.util.Collections;
  */
 public class Achievement implements Serializable
 {
+    private static final long serialVersionUID = 1L;
     private String name;
     private int points;
     private String imageName;
