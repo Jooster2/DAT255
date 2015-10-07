6bff7fe61bf692a378c7213680bfdb62bbce7e73
Jesper <jeskje@student.chalmers.se>
2015-10-07 14:24:53 +0200

    ListView working!

--- app/src/main/java/com/soctec/soctec/core/AchievementsFragment.java
+++ app/src/main/java/com/soctec/soctec/core/AchievementsFragment.java
@@ -11,9 +11,13 @@ import android.view.View;
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
+import android.widget.ListView;
 import android.widget.ViewSwitcher;
 
 import com.soctec.soctec.R;
+import com.soctec.soctec.achievements.AchievementsAdapter;
+
+import java.util.ArrayList;
 
 public class AchievementsFragment extends Fragment
 {
@@ -25,6 +29,26 @@ public class AchievementsFragment extends Fragment
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
     {
         View view = inflater.inflate(R.layout.fragment_achievements, container, false);
+
+        ArrayList<String> theList =  new ArrayList<String> ();
+        ListView unlockedAchievementListView;
+        theList.add("Hej, someimg,extratext");
+        theList.add("på");
+        theList.add("dig");
+        theList.add("jesper!");
+        unlockedAchievementListView = (ListView)view.findViewById(R.id.listunlocked);
+        AchievementsAdapter unlockedadapter = new AchievementsAdapter(getActivity(), theList);
+        unlockedAchievementListView.setAdapter(unlockedadapter);
+
+        ArrayList<String> theOtherList =  new ArrayList<String> ();
+        ListView lockedAchievmentListView;
+        theOtherList.add("prutt, someimg,extratext");
+        theOtherList.add("på");
+        theOtherList.add("dig");
+        theOtherList.add("jesper!");
+        lockedAchievmentListView = (ListView)view.findViewById(R.id.listlocked);
+        AchievementsAdapter lockedadapter = new AchievementsAdapter(getActivity(), theOtherList);
+        lockedAchievmentListView.setAdapter(lockedadapter);
 
         unlocked = (ImageButton) view.findViewById(R.id.unlocked);
         locked = (ImageButton) view.findViewById(R.id.locked);
