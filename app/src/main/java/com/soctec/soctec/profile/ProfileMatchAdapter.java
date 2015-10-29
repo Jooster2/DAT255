2ab8b15ba4a23eff36dc582660b61515de6ac07e
RallaRen <chult@student.chalmers.se>
2015-10-29 15:13:26 +0100

    added comments

--- app/src/main/java/com/soctec/soctec/profile/ProfileMatchAdapter.java
+++ app/src/main/java/com/soctec/soctec/profile/ProfileMatchAdapter.java
@@ -5,8 +5,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.EditText;
-import android.widget.ImageButton;
 import android.widget.TextView;
 
 import com.soctec.soctec.R;
